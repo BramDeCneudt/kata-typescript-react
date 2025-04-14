139edec8871d3745eadae6feae5e386a4efb9d7a
Bram De Cneudt <bram@icodeit.be>
2025-04-14 22:30:26 +0200

    kata-1 create test cases for existing code
    - added golden tests
    - added unit tests for all different existing item types and
      configurations

--- test/vitest/approvals.spec.ts
+++ test/vitest/approvals.spec.ts
@@ -1,6 +1,6 @@
 import { execSync } from 'node:child_process';
 import { Item, GildedRose } from '@/lib/gilded-rose';
-import { describe, expect, it } from 'vitest';
+import { describe, expect, test } from 'vitest';
 
 /**
  * This test uses Vitest Snapshot, similar to [Jest Snapshot](https://goo.gl/fbAQLP).
@@ -12,20 +12,42 @@ import { describe, expect, it } from 'vitest';
  * I suggest choosing one style to develop and deleting the other.
  */
 
-describe('Gilded Rose Approval', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-    const items = gildedRose.updateQuality();
+const items : Item[] = [
+    new Item("+5 Dexterity Vest", 10, 0), //
+    new Item("+6 Dexterity Vest", -1, 10), //
+    new Item("Aged Brie", 2, 0), //
+    new Item("Aged Brie", 2, 50), //
+    new Item("Elixir of the Mongoose", 5, 7), //
+    new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+    new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+    new Item("Backstage passes to a TAFKAL80ETC concert", 15, 50),
+    new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+    new Item("Backstage passes to a TAFKAL80ETC concert", 10, 50),
+    new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20),
+    new Item("Backstage passes to a TAFKAL80ETC concert", 5, 20),
+    new Item("Backstage passes to a TAFKAL80ETC concert", 5, 50),
+    new Item("Backstage passes to a TAFKAL80ETC concert", 0, 49),
+    new Item("Conjured Mana Cake", 3, 6),
+    new Item("Conjured Mana Cake", -3, 6),
+    new Item("Conjured Mana Cake", -3, 3)
+]
 
-    expect(items).toMatchSnapshot();
+describe('Gilded Rose Golden Test', () => {
+  test('Golden Test base test', () => {
+    const gildedRose : GildedRose = new GildedRose(items);
+    const actualItems = gildedRose.updateQuality();
+
+    expect(actualItems).toMatchSnapshot();
   });
 
-  it('should thirtyDays', () => {
-    const consoleOutput = execSync(
-      'ts-node test/golden-master-text-test.ts 30',
-      { encoding: 'utf-8' }
-    );
+  test('Golden Test test after 5 days', () => {
+    const gildedRose : GildedRose = new GildedRose(items);
+    let actualItems;
+    for (let day = 0; day <= 5; day++) {
+      actualItems = gildedRose.updateQuality() ;
+    }
 
-    expect(consoleOutput).toMatchSnapshot();
+    expect(actualItems).toMatchSnapshot();
   });
+
 });
--- test/vitest/gilded-rose.spec.ts
+++ test/vitest/gilded-rose.spec.ts
@@ -1,10 +1,205 @@
 import { Item, GildedRose } from '@/lib/gilded-rose';
-import { describe, expect,  it} from 'vitest';
+import { describe, expect, test } from 'vitest';
 
-describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+describe('Normal items', () => {
+  test('with positive sellIn should degrade normally', () => {
+    const itemName = "Normal Item";
+    const items: Item[] = [new Item(itemName, 5, 20)];
+    const app: GildedRose = new GildedRose(items);
+
+    app.updateQuality();
+
+    expect(itemName).toBe(app.items[0].name);
+    expect(19).toBe(app.items[0].quality);
+    expect(4).toBe(app.items[0].sellIn);
   });
+
+
+  test('with almost positive sellIn should degrade normally', () => {
+    const itemName = "Normal Item";
+    const items: Item[] = [new Item(itemName, 1, 20)];
+    const app: GildedRose = new GildedRose(items);
+
+    app.updateQuality();
+
+    expect(itemName).toBe(app.items[0].name);
+    expect(19).toBe(app.items[0].quality);
+    expect(0).toBe(app.items[0].sellIn);
+  });
+
+  test('with zero sellIn should degrade twice as fast', () => {
+    const itemName = "Degraded Item";
+    const items: Item[] = [new Item(itemName, 0, 20)];
+    const app: GildedRose = new GildedRose(items);
+
+    app.updateQuality();
+
+    expect(itemName).toBe(app.items[0].name);
+    expect(18).toBe(app.items[0].quality);
+    expect(-1).toBe(app.items[0].sellIn);
+  });
+
+  test('with zero quality but positive sellIn then quality should remain zero', () => {
+    const itemName = "Zero Quality Item";
+    const items: Item[] = [new Item(itemName, 5, 0)];
+    const app: GildedRose = new GildedRose(items);
+
+    app.updateQuality();
+
+    expect(itemName).toBe(app.items[0].name);
+    expect(0).toBe(app.items[0].quality);
+    expect(4).toBe(app.items[0].sellIn);
+  });
+
+
+  test('with zero quality and negative sellIn the quality should remain at zero', () => {
+    const itemName = "Zero Degraded Quality Item";
+    const items: Item[] = [new Item(itemName, -5, 0)];
+    const app: GildedRose = new GildedRose(items);
+
+    app.updateQuality();
+
+    expect(itemName).toBe(app.items[0].name);
+    expect(0).toBe(app.items[0].quality);
+    expect(-6).toBe(app.items[0].sellIn);
+  });
+});
+
+
+describe('Aged Brie Items', () => {
+
+  test('with postive sellIn should increase quality by 1', () => {
+    const itemName = "Aged Brie";
+    const items: Item[] = [new Item(itemName, 5, 20)];
+    const app: GildedRose = new GildedRose(items);
+
+    app.updateQuality();
+
+    expect(itemName).toBe(app.items[0].name);
+    expect(21).toBe(app.items[0].quality);
+    expect(4).toBe(app.items[0].sellIn);
+  });
+
+
+  test('with negative sellIn should increase quality by 2', () => {
+    const itemName = "Aged Brie";
+    const items: Item[] = [new Item(itemName, -5, 20)];
+    const app: GildedRose = new GildedRose(items);
+
+    app.updateQuality();
+
+    expect(itemName).toBe(app.items[0].name);
+    expect(22).toBe(app.items[0].quality);
+    expect(-6).toBe(app.items[0].sellIn);
+  });
+
+  test('with maximum quality then quality should not increase', () => {
+    const itemName = "Aged Brie";
+    const items: Item[] = [new Item(itemName, 5, 50)];
+    const app: GildedRose = new GildedRose(items);
+
+    app.updateQuality();
+
+    expect(itemName).toBe(app.items[0].name);
+    expect(50).toBe(app.items[0].quality);
+    expect(4).toBe(app.items[0].sellIn);
+  });
+
+});
+
+
+describe('Legendary Items', () => {
+
+  test('with postive sellIn then quality should stay the same', () => {
+    const itemName = "Sulfuras, Hand of Ragnaros";
+    const items: Item[] = [new Item(itemName, 4, 80)];
+    const app: GildedRose = new GildedRose(items);
+
+    app.updateQuality();
+
+    expect(itemName).toBe(app.items[0].name);
+    expect(80).toBe(app.items[0].quality);
+    expect(4).toBe(app.items[0].sellIn);
+  });
+
+
+  test('with zero sellIn then quality should stay the same', () => {
+    const itemName = "Sulfuras, Hand of Ragnaros";
+    const items: Item[] = [new Item(itemName, 0, 80)];
+    const app: GildedRose = new GildedRose(items);
+
+    app.updateQuality();
+
+    expect(itemName).toBe(app.items[0].name);
+    expect(80).toBe(app.items[0].quality);
+    expect(0).toBe(app.items[0].sellIn);
+  });
+
+});
+
+
+describe('Backstage Passes', () => {
+
+  test('with more then 10 sellIn should increase quality by 1', () => {
+    const itemName = "Backstage passes to a TAFKAL80ETC concert";
+    const items: Item[] = [new Item(itemName, 15, 40)];
+    const app: GildedRose = new GildedRose(items);
+
+    app.updateQuality();
+
+    expect(itemName).toBe(app.items[0].name);
+    expect(41).toBe(app.items[0].quality);
+    expect(14).toBe(app.items[0].sellIn);
+  });
+
+
+  test('with 10 sellIn should increase quality by 2', () => {
+    const itemName = "Backstage passes to a TAFKAL80ETC concert";
+    const items: Item[] = [new Item(itemName, 10, 40)];
+    const app: GildedRose = new GildedRose(items);
+
+    app.updateQuality();
+
+    expect(itemName).toBe(app.items[0].name);
+    expect(42).toBe(app.items[0].quality);
+    expect(9).toBe(app.items[0].sellIn);
+  });
+
+  test('with 5 sellIn should increase quality by 3', () => {
+    const itemName = "Backstage passes to a TAFKAL80ETC concert";
+    const items: Item[] = [new Item(itemName, 5, 40)];
+    const app: GildedRose = new GildedRose(items);
+
+    app.updateQuality();
+
+    expect(itemName).toBe(app.items[0].name);
+    expect(43).toBe(app.items[0].quality);
+    expect(4).toBe(app.items[0].sellIn);
+  });
+
+  test('with 5 sellIn but almost maximum quality should increase quality by 2 to maximum quality', () => {
+    const itemName = "Backstage passes to a TAFKAL80ETC concert";
+    const items: Item[] = [new Item(itemName, 5, 48)];
+    const app: GildedRose = new GildedRose(items);
+
+    app.updateQuality();
+
+    expect(itemName).toBe(app.items[0].name);
+    expect(50).toBe(app.items[0].quality);
+    expect(4).toBe(app.items[0].sellIn);
+  });
+
+  test('with 0 sellIn should drop quality to 0', () => {
+    const itemName = "Backstage passes to a TAFKAL80ETC concert";
+    // TODO kata-5 is this correct? Should drop to 0 already at day 0?
+    const items: Item[] = [new Item(itemName, -1, 50)];
+    const app: GildedRose = new GildedRose(items);
+
+    app.updateQuality();
+
+    expect(itemName).toBe(app.items[0].name);
+    expect(0).toBe(app.items[0].quality);
+    expect(-2).toBe(app.items[0].sellIn);
+  });
+
 });
